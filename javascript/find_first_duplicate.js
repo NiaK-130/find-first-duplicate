c5f7821199221c6c611d1f9420db720c8ca447c9
Nia K <nsjkappen@gmail.com>
2022-01-17 05:52:13 -0800

    updated

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,8 +1,23 @@
 function findFirstDuplicate(arr) {
-  // type your code here sdfs
+  // 1. go through each num in array and 
+  // see if the num is like the num after 
+  // will need to do for loop 
+  // also an if conditional 
+  //2. Input: [2, 1, 3, 3, 2]
+   //  Output: 3
+   // 3. for (let i = 0, i < arr.length, i ++){
+     // if ()
 
-  sdfsfs 
-}
+     const mySet = new Set()
+     for (let i = 0; i < arr.length; i ++){
+       if (mySet.has(arr[i]))
+        return arr[i];
+        mySet.add(arr[i]);  
+     }
+     return -1 
+  
+   }
+
 
 if (require.main === module) {
   // add your own tests in here
